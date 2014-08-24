27082df06cfbb15e641fa46dd7fcaeaf84d2885b
hirekris <khebert@Kristoffers-MacBook-Pro.local>
2014-08-23 18:51:02 -0700

    finished animations assignment for waitstaff assignment

--- waitstaff-routes-animated/main.js
+++ waitstaff-routes-animated/main.js
@@ -1,4 +1,17 @@
-angular.module('waitStaff', ['ngRoute'])
+angular.module('waitStaff', ['ngRoute', 'ngAnimate'])
+    .run(function($rootScope, $location, $timeout) {
+        $rootScope.$on('$routeChangeError', function() {
+            $location.path("/error");
+        });
+        $rootScope.$on('$routeChangeStart', function() {
+            $rootScope.isLoading = true;
+        });
+        $rootScope.$on('$routeChangeSuccess', function() {
+            $timeout(function() {
+                $rootScope.isLoading = false;
+            }, 200);
+        });
+    })
     .config(function($routeProvider) {
         $routeProvider.when('/', {
             templateUrl: 'home.html',
