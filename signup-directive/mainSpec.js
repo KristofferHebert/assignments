961928ed45c3715ea19adeb97c1266d2a4c2e00b
hirekris <khebert@Kristoffers-MacBook-Pro.local>
2014-09-04 18:55:38 -0700

    added tests for directive

---
+++ signup-directive/mainSpec.js
@@ -0,0 +1,27 @@
+describe('Testing for signup directive', function () {
+    var element, scope, input;
+
+    beforeEach(module('app'));
+    beforeEach(module('opt-in.html'));
+
+    beforeEach(inject(function ($rootScope, $compile) {
+        element = angular.element(
+            '<form action="#">' +
+            '<div ng-transclude></div>' +
+            '<input type="text" placeholder="First Name">' +
+            '<input type="text" placeholder="Last Name">' +
+            '<input type="email" placeholder="Email">' +
+            '<input type="submit" vale="Submit" />' +
+            '</form>');
+        scope = $rootScope;
+        $compile(element)(scope);
+        scope.digest();
+    }));
+
+    it("should have the correct amount of inputs", function () {
+        input = element.find('input');
+        expect(input.length)
+            .toBe(4);
+    });
+
+});
