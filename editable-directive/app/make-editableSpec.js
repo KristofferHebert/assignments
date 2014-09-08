15f3b6d11f4f4ebea7737953d4e73586d6674ec2
hirekris <khebert@Kristoffers-MacBook-Pro.local>
2014-09-07 19:44:27 -0700

    Added message

--- editable-directive/app/make-editableSpec.js
+++ editable-directive/app/make-editableSpec.js
@@ -1,1 +1,23 @@
-//
+describe('Testing for signup directive', function () {
+    var element, scope, directive, compiled, html;
+
+    beforeEach(module('app'));
+    beforeEach(module('make-editable.html'));
+    beforeEach(inject(function ($rootScope, $compile) {
+        html = '<div make-editable>' +
+            '<p>Content that will become editable when the directive rendered.</p>' +
+            '</div>';
+
+        scope = $rootScope.$new();
+        compiled = $compile(html);
+        element = compiled(scope);
+        scope.$digest();
+    }));
+
+    it("should have the correct amount of inputs", function () {
+        div = element.find('div');
+        expect(div.length)
+            .toBe(1);
+    });
+
+});
